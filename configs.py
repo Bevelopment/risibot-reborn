a6bed0dd8dd6f1008489347a9af7470842ce15b0
Benftw <benftwc@gmail.com>
2022-12-10 01:22:08 +0100

    Fix requirements and add PYTZ

--- configs.py
+++ configs.py
@@ -6,9 +6,9 @@ BOT_TOKEN = config["BOT_TOKEN"]
 STATUS_TASK = config["STATUS_TASK"]
 GUILD_TEST_ID = int(config["GUILD_TEST_ID"] if config["GUILD_TEST_ID"] else 0)
 
-CLR_SUCCESS = int(f"0x{config['CLR_SUCCESS']}", 16)
-CLR_ERROR = int(f"0x{config['CLR_ERROR']}", 16)
-CLR_WARNING = int(f"0x{config['CLR_WARNING']}", 16)
+CLR_SUCCESS = int(f"{config['CLR_SUCCESS']}", 16)
+CLR_ERROR = int(f"{config['CLR_ERROR']}", 16)
+CLR_WARNING = int(f"{config['CLR_WARNING']}", 16)
 
 LOGS_CHANNEL = int(f"{config['LOGS_CHANNEL']}")
 ARCHIVE_CHANNEL = int(f"{config['ARCHIVE_CHANNEL']}")
