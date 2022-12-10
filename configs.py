7636f4f3c93f10682b68cab0f3c6183e04ddd906
Benftw <benftwc@gmail.com>
2022-12-10 01:05:42 +0100

    Add moderation commands

--- configs.py
+++ configs.py
@@ -5,3 +5,7 @@ config = dotenv_values(".env")  # Makes a dict out of the values.
 BOT_TOKEN = config["BOT_TOKEN"]
 STATUS_TASK = config["STATUS_TASK"]
 GUILD_TEST_ID = int(config["GUILD_TEST_ID"] if config["GUILD_TEST_ID"] else 0)
+
+CLR_SUCCESS = config["CLR_SUCCESS"]
+CLR_ERROR = config["CLR_ERROR"]
+CLR_WARNING = config["CLR_WARNING"]
